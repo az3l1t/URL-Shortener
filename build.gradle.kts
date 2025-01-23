f76f41c439250bd87a5b86ad62950790ffc99332
az3l1t <eternity_cr9p@mail.ru>
2025-01-23 18:16:38 +0300

    ci and jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "net.azelit"
 version = "0.0.1-SNAPSHOT"
+
+jacoco {
+	toolVersion = "0.8.7"
+}
 
 java {
 	toolchain {
@@ -36,4 +41,47 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	testLogging.showStandardStreams = true
+}
+
+val test by tasks.getting(Test::class) { }
+
+tasks.jacocoTestCoverageVerification {
+	dependsOn(tasks.test)
+	violationRules {
+		rule {
+			limit {
+				minimum = 0.6.toBigDecimal()
+			}
+		}
+	}
+}
+
+tasks.withType<JacocoReport> {
+	val filteredClassDirectories = classDirectories.files.map { dir ->
+		project.fileTree(dir) {
+			exclude(
+				"net/azelit/user_service/client",
+				"net/azelit/user_service/config",
+				"net/azelit/user_service/controller",
+				"net/azelit/user_service/dto",
+				"net/azelit/user_service/mapper"
+			)
+		}
+	}
+	classDirectories.setFrom(filteredClassDirectories)
+}
+
+tasks {
+	val jacocoCustomTestReport by creating(JacocoReport::class) {
+		reports {
+			xml.isEnabled = false
+			csv.isEnabled = false
+			html.isEnabled = true
+		}
+	}
+
+	withType<Test> {
+		finalizedBy(jacocoCustomTestReport)
+	}
 }
